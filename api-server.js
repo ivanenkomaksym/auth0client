5f46ebdad5766637d21e6ece2b3108a14b911f2b
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-03-26 16:52:05 +0200

    Make config.js the only provider of config options. Make domain, audience and port part of each .env and config.

--- api-server.js
+++ api-server.js
@@ -4,21 +4,15 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const axios = require('axios');
 const { auth } = require("express-oauth2-jwt-bearer");
-const authConfig = require("./src/auth_config.json");
+const { getConfig } = require("./src/config");
 
 const app = express();
 
-process.env.REACT_APP_SERVER_PORT = process.env.PORT;
-console.log("REACT_APP_API_PORT", process.env.REACT_APP_API_PORT);
-console.log("REACT_APP_SERVER_PORT", process.env.REACT_APP_SERVER_PORT);
-const port = process.env.REACT_APP_API_PORT || 3001;
-const appPort = process.env.REACT_APP_SERVER_PORT || 3000;
-const appOrigin = authConfig.appOrigin || `http://localhost:${appPort}`;
+const config = getConfig();
 
 if (
-  !authConfig.domain ||
-  !authConfig.audience ||
-  authConfig.audience === "YOUR_API_IDENTIFIER"
+  !config.domain ||
+  !config.audience
 ) {
   console.log(
     "Exiting: Please make sure that auth_config.json is in place and populated with valid domain and audience values"
@@ -29,11 +23,11 @@ if (
 
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(cors({ origin: appOrigin }));
+app.use(cors({ origin: config.appOrigin }));
 
 const checkJwt = auth({
-  audience: authConfig.audience,
-  issuerBaseURL: `https://${authConfig.domain}/`,
+  audience: config.audience,
+  issuerBaseURL: `https://${config.domain}/`,
   algorithms: ["RS256"],
 });
 
@@ -106,8 +100,8 @@ app.get('/groups/:groupId/users', async (req, res) => {
 });
 
 const checkWeatherForecastJwt = auth({
-  audience: "http://localhost:3000/external-api/weatherforecast",
-  issuerBaseURL: `https://${authConfig.domain}/`,
+  audience: "https://easyclean.contoso.com",
+  issuerBaseURL: `https://${config.domain}/`,
   algorithms: ["RS256"],
 });
 
@@ -117,4 +111,4 @@ app.get("/api/weatherforecast", checkWeatherForecastJwt, (req, res) => {
   });
 });
 
-app.listen(port, () => console.log(`API Server listening on port ${port}`));
+app.listen(config.port, () => console.log(`API Server listening on port ${config.port}`));
--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-import configJson from "./auth_config.json";
-
 export function getConfig() {
   // Configure the audience here. By default, it will take whatever is in the config
   // (specified by the `audience` key) unless it's the default value of "YOUR_API_IDENTIFIER" (which
@@ -7,18 +5,26 @@ export function getConfig() {
   // don't have an API).
   // If this resolves to `null`, the API page changes to show some helpful info about what to do
   // with the audience.
-  const audience =
-    configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
-      ? configJson.audience
-      : null;
 
   console.log("REACT_APP_CLIENT_ID", process.env.REACT_APP_CLIENT_ID);
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+
+  process.env.REACT_APP_SERVER_PORT = process.env.PORT;
+  console.log("REACT_APP_API_PORT", process.env.REACT_APP_API_PORT);
+  console.log("REACT_APP_SERVER_PORT", process.env.REACT_APP_SERVER_PORT);
+  const port = process.env.REACT_APP_API_PORT || 3001;
+  const appPort = process.env.REACT_APP_SERVER_PORT || 3000;
+  const appOrigin = `http://localhost:${appPort}`;
+  const domain = process.env.REACT_APP_DOMAIN;
+  const audience = process.env.REACT_APP_AUDIENCE;
+  console.log("domain", domain);
   console.log("audience", audience);
-  const clientId = process.env.REACT_APP_CLIENT_ID || configJson.clientId;
 
   return {
-    domain: configJson.domain,
+    domain: domain,
     clientId: clientId,
     ...(audience ? { audience } : null),
+    port: port,
+    appOrigin: appOrigin
   };
 }
